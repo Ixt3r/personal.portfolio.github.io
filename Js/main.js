51877e5e8112dc4a08c50c688edf1d721d8bf55e
Estefani <mra.estefani1@gmail.com>
2023-09-13 21:51:01 -0300

    atualização do menu mobile - show - remove on clink

--- Js/main.js
+++ Js/main.js
@@ -7,10 +7,23 @@ if (navToggle) {
     navMenu.classList.add("show-menu");
   });
 }
+//
 /*menu hidden*/
+//
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
   });
   /*20,08*/
 }
+//
+/*Remove menu mobile*/
+//
+const navLink = document.querySelectorAll("nav-link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  //when we clink on each nav-link we remove the show menu
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
