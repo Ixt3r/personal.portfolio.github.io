b21279deb5e3af8ab8558714f9e174dbd4830f99
Estefani <mra.estefani1@gmail.com>
2023-10-03 21:04:49 -0300

    scroll down & scroll up

--- Js/main.js
+++ Js/main.js
@@ -37,3 +37,36 @@ const shadowHeader = () => {
     : header.classList.remove("shadow-header");
 };
 window.addEventListener("scroll", shadowHeader);
+/*------E-MAIL-----------*/
+/*------Show scroll up ---------*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  //when the scroll is higher than 350Vh, add the show-scroll
+  //class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+/*-------scroll sections active link---------*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav-menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
